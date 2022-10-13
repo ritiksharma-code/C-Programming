6d4d4b0af118a1c5c895cba12fc37534a9252753
ritiksharma-code <73885960+ritiksharma-code@users.noreply.github.com>
2022-10-13 21:23:41 +0530

    Add files via upload

---
+++ Assignment 10/A10_Q8.c
@@ -0,0 +1,14 @@
+// Author: Ritik Sharma
+
+#include <stdio.h>
+int main()
+{
+    int i;
+    float f;
+    printf("Testing of various arithmetic operations\n");
+    i=2.0/5.0;
+    f=2.0/5.0;
+    printf("value of integer i is %d.\n", i);       //2
+    printf("Value of float f is %d.\n", f);         ////-1610612736
+    return 0;
+}

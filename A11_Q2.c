fd065a71a897ca27e96c91b1bdde7ac028d73c38
ritiksharma-code <73885960+ritiksharma-code@users.noreply.github.com>
2022-11-09 21:31:10 +0530

    Add files via upload

---
+++ A11_Q2.c
@@ -0,0 +1,11 @@
+// Author: Ritik Sharma
+
+#include <stdio.h>
+int main()
+{
+
+    int a=0;
+    double d=10.21;
+    printf("%lu\n", sizeof(a+d));       //8
+    return 0;
+}
---
+++ A11_Q8.c
@@ -0,0 +1,12 @@
+// Author: Ritik Sharma
+
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    for(a=10;a>=0;a--)
+    {
+        printf("%d ", a);
+    }
+    return 0;
+}

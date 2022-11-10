43b1d4a85b1c087b707e5cebc90c4c97d2b31af5
ritiksharma-code <73885960+ritiksharma-code@users.noreply.github.com>
2022-11-10 20:24:30 +0530

    Add files via upload

---
+++ Assignment 12/A12_Q10.c
@@ -0,0 +1,19 @@
+// Author: Ritik Sharma
+
+#include <stdio.h>
+int main()
+{
+    int a;    
+    
+    for(a=1;a<=20;a++)
+    {
+        if(a%2==0)
+        {
+            printf("%d ",a);
+        }            
+        else
+        {
+        }
+    }
+    return 0;
+}
---
+++ Assignment 12/A12_Q9.c
@@ -0,0 +1,19 @@
+// Author: Ritik Sharma
+
+#include <stdio.h>
+int main()
+{
+    int a;    
+    
+    for(a=19;a>=1;a--)
+    {
+        if(a%2==0)
+        {
+        }
+        else
+        {
+            printf("%d ",a);
+        }
+    }
+    return 0;
+}

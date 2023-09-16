3242a52f7ebdbb56d56a3876b95f7ab1178c8bcf
ritiksharma-code <ritiksharma0405@gmail.com>
2023-09-16 23:32:45 +0530

    update

--- Practice/freecodecamp/file_handling_read.c
+++ Practice/freecodecamp/file_handling_read.c
@@ -5,6 +5,7 @@ int main()
     char line[255];
     FILE * fpointer = fopen("employees.txt", "r");
     fgets(line, 255, fpointer);
+    fgets(line, 255, fpointer);
     printf("%s", line);
     fclose(fpointer);
 }

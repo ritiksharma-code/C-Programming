7bdf442edbb227949ecf8618930e2b44b8c7e394
ritiksharma-code <ritiksharma0405@gmail.com>
2023-09-17 18:50:43 +0530

    update

--- Practice/freecodecamp/file_handling.c
+++ Practice/freecodecamp/file_handling.c
@@ -7,7 +7,8 @@ int main()
     // FILE * fpointer = fopen("employees.txt", "w");
     FILE * fpointer = fopen("employees.txt", "a");
     // w is gonna overwrite all the text data in the file so if you want to keep the data and add something more then you have to use (append)
-    fprintf(fpointer, "\nManas Parwani, Student\nHarshal Jain, Student");
+    // fprintf(fpointer, "\nManas Parwani, Student\nHarshal Jain, Student");
+    fprintf(fpointer, "\nManasvi Sharma, Student");
 
     fclose(fpointer);
 }
